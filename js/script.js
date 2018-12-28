5165adf61135105434ddf6357c6820e4b1f7c9b7
Shannon Russell <s.russell3271@gmail.com>
2018-12-28 00:17:21 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -11,22 +11,11 @@ var question6Points;
 var question7Points;
 var question8Points;
 var question9Points;
-
-// var totalScore;
-
-$(document).ready(function() {
-    // $("button").click(function() {
-    //     var name = $("#name").val();
-    //     var techPlacement = "TBD";
-    //     var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-    //     var q1Result = $("#question1").val();
-    //     var q2Result = $("#question2").val();
-    //     var q3Result = $("#question3").val();
-    //     // var totalScore;
-    // });
+var question10Points;
+var totalPoints;
     
     
-function pointsForQuestion1(){
+function pointsForQuestion(){
     var userInput1= $("#question1").val();
     if (userInput1 === "cat" || userInput1 === "a cat"){
         question1Points= points * 3;
@@ -37,9 +26,7 @@ function pointsForQuestion1(){
     else if (userInput1 === "hamster" ||userInput1 === "a hamster"){
         question1Points= points;
        }
-    }
-    
-function pointsForQuestion2(){
+
     var userInput2= $("#question2").val();
     if (userInput2 === "chicken" || userInput2 === "Chicken"){
         question2Points= points * 3;
@@ -50,9 +37,7 @@ function pointsForQuestion2(){
     else if (userInput2 === "beef" ||userInput2 === "Beef"){
         question2Points= points;
        }
-    }
-    
-function pointsForQuestion3(){
+
     var userInput3= $("#question3").val();
     if (userInput3 === "sleeping" || userInput3 === "Sleeping"){
         question3Points= points * 5;
@@ -66,29 +51,27 @@ function pointsForQuestion3(){
     else if (userInput3 === "reading" ||userInput3 === "Reading"){
         question3Points= points * 2;
        }
-    }
 
-// function pointsForQuestion4(){
-//     var userInput4= $("#question4").val();
-//     var myGrade = "11";
-//     var guesses = Math.floor(Math.random())* 13;
-//     if (userInput4 === myGrade && userInput4 === guesses){
-//         question4Points= points * 5;
-//       }
-//     else if(userInput4 > "11" && userInput4> guesses){
-//         question4Points= points * 6;
-//       }
-//     else if(userInput4 < "11"  && userInput4< guesses){
-//         question4Points= points * 4;
-//       }
+    var userInput4= $("#question4").val();
+    var myGrade = 11;
+    
+    if (parseInt(userInput4) === myGrade){
+        question4Points= points * 5;
+      }
+    else if(parseInt(userInput4) > 11 ){
+        question4Points= points * 6;
+      }
+    else if(parseInt(userInput4) < 11 ){
+        question4Points= points * 4;
+      }
 
-
-function pointsForQuestion5(){
-    var userInput5= $("#question5").val();
-    if (userInput5 === "volleyball" || userInput5 === "basketball"){
+    
+    var userInput5 = $("#question5").val(); 
+    
+    if (userInput5 === "volleyball" ||userInput5 === "basketball"){
         question5Points= points * 5;
        }
-    else if(userInput5 === "soccer" || userInput5 === "baseball"){
+    else if(userInput5 === "soccer" ||userInput5 === "baseball"){
         question5Points= points * 4;
        }
     else if (userInput5 === "rugby" ||userInput5 === "track"){
@@ -97,70 +80,86 @@ function pointsForQuestion5(){
     else if (userInput5 === "football" ||userInput5 === "cricket"){
         question5Points= points * 2;
        }
-    }
-function pointsForQuestion6(){
+    else{
+        question5Points= points * 6;
+        }
+    
+
     var userInput6= $("#question6").val();
-    if (userInput6 === "Andriod" || userInput6 === "andriod"){
+    if (userInput6 === "Android" || userInput6 === "android"){
         question6Points= points * 5;
-       }
+      }
     else if(userInput6 === "iphone" || userInput6 === "Iphone"){
         question6Points= points * 10;
-       }
-}
-    
-function pointsForQuestion7(){
+      }
+
     var userInput7= $("#question7").val();
     if (userInput7 === "Drama" || userInput7 === "drama"){
         question7Points= points * 5;
-       }
+      }
     else if(userInput7 === "Action" || userInput7 === "action"){
         question7Points= points * 10;
-       }
-}
-  
-// function pointsForQuestion8(){
-//     var userInput8= $("#question8").val();
-//     if(userInput8===$("#question8").val){
-//          question8Points = points *7;
-//     }
-// }  
+      }
 
-function pointsForQuestion9(){
+    var userInput8= question8Points;
+        question8Points = points *7;
+    
+
     var userInput9= $("#question8").val();
-    console.log($("#question8").val());
-    if (userInput9 === "" || userInput9 === "2"){
+    
+    if (parseInt(userInput9) === 1 || parseInt(userInput9) === 2){
         question9Points= points * 2;
-       }
-    else if(userInput9 === "3" || userInput9 === "4"){
+      }
+    else if(parseInt(userInput9) === 3 || parseInt(userInput9) === 4){
         question9Points= points * 4;
-       }
-    else if(userInput9 === 5 || userInput9 === "6"){
+    }
+    else if(parseInt(userInput9) === 5 || parseInt(userInput9) === 6){
         question9Points= points * 6;
-       }
-    else if(userInput9 === "7" || userInput9 === "8"){
+      }
+    else if(parseInt(userInput9) === 7 || parseInt(userInput9) === 8){
         question9Points= points * 6;
-       }
+      }
     else {
         question9Points= points * 7;
-       }
-}
-$("button").click(function(){ 
-    pointsForQuestion1();
-    pointsForQuestion2();
-    pointsForQuestion3();
-    // pointsForQuestion4();
-    pointsForQuestion5();
-    pointsForQuestion6();
-    pointsForQuestion7();
-    // pointsForQuestion8();
-    pointsForQuestion9();
+      }
 
-    console.log(question9Points);
-    console.log(question1Points + question2Points + question3Points + question5Points
-    + question6Points + question7Points + question9Points );
-// question4Points + question8Points + 
+    var userInput10= question10Points;
+        question10Points = points *15;
+        
+    totalPoints = question1Points + question2Points + question3Points +question4Points + question5Points + question6Points+ question7Points + question9Points + question10Points;
+   
+} 
+
+
+$("button").click(function(){ 
+    pointsForQuestion();
+    totalPoints = question1Points + question2Points + question3Points +question4Points + question5Points + question6Points+ question7Points + question9Points + question10Points;
+    
+    var names = $("#name").val();
+
+
+    if (totalPoints <40){
+        alert("You " + names + " work In McDonalds");
+        }
+    else if(totalPoints >40 && totalPoints < 50){
+        alert("You " + names + " work In Wendy's");
+        }
+
+    else if(totalPoints >50 && totalPoints < 60){
+        alert("You " + names + " will work as A Secuirty Guard");
+        }
+    else if(totalPoints >60 && totalPoints < 70){
+        alert("You " + names + " work In Saleforce");
+        }
+    else if(totalPoints >70 && totalPoints < 80){
+        alert("You " + names + " work for  Google");
+        }
+    else if(totalPoints >80 ){
+        alert("You " + names + " work for  anything you want");
+        }
+
+
+
     });
     
     
-});
-
